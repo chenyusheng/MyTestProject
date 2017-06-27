de2e6cb53e266bf872e16a6b3941c7dc319f119b
yusheng <yusheng.chan@shendun.info>
2017-06-27 17:40:26 +0800

    提交

---
+++ app/src/main/java/yeyeapp/in/mytestproject/MyApplication.java
@@ -0,0 +1,21 @@
+package yeyeapp.in.mytestproject;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ *     author : mac
+ *     e-mail : 465680056@qq.com
+ *     time   : 2017/06/27
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
